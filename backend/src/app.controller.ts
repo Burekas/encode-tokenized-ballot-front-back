9fdc6976c9bda93ef9a524145a0d99b86c041df8
Ilya Tsivkovsky <baakjira@skiff.com>
2023-11-20 04:02:09 +0200

    get ballot contract and proposals from backend

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Body, Post } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MintTokenDto } from './dtos/MintToken.dto';
 
@@ -6,14 +6,24 @@ import { MintTokenDto } from './dtos/MintToken.dto';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('contract-address')
-  getContractAddress(){
-    return {address: this.appService.getContractAddress()};
+  @Get('token-contract-address')
+  getTokenContractAddress(){
+    return {address: this.appService.getTokenContractAddress()};
+  }
+
+  @Get('ballot-contract-address')
+  getBallotContractAddress(){
+    return {address: this.appService.getBallotContractAddress()};
   }
 
   @Post('mint-tokens')
   async mintTokens(@Body() body: MintTokenDto ) {
     return {hash: await this.appService.mintTokens(body.signature)};
+  }
+
+  @Get('proposals')
+  async getProposals() {
+    return { proposals: await this.appService.getProposals() };
   }
 }
 
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ethers } from 'ethers';
 const { hashMessage } = require("@ethersproject/hash");
 import * as tokenJson from './assets/MyToken.json';
+import * as ballotJson from './assets/TokenizedBallot.json';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -10,6 +11,7 @@ export class AppService {
   contract: ethers.Contract;
   provider: ethers.Provider;
   wallet: ethers.Wallet;
+  ballotContract: ethers.Contract;
 
   constructor(private configService: ConfigService) {
     this.provider = new ethers.JsonRpcProvider(
@@ -24,10 +26,19 @@ export class AppService {
       tokenJson.abi,
       this.wallet,
     );
+    this.ballotContract = new ethers.Contract(
+      this.configService.get<string>('BALLOT_ADDRESS'),
+      ballotJson.abi,
+      this.wallet,
+    );
   }
 
-  getContractAddress(): string {
-    return process.env.TOKEN_ADDRESS;
+  getTokenContractAddress(): string {
+    return this.configService.get<string>('TOKEN_ADDRESS');
+  }
+
+  getBallotContractAddress(): string {
+    return this.configService.get<string>('BALLOT_ADDRESS');
   }
 
   async mintTokens(signature: string) {
@@ -35,5 +46,14 @@ export class AppService {
     const signer = ethers.recoverAddress(hashMessage(message), signature);
     const mintTx = await this.contract.mint(signer, ethers.parseUnits('100000000000000000'));
     return mintTx.hash;
+  }
+
+  async getProposals() {
+    const rawProposals = await this.ballotContract.getProposals();
+    let proposals: string[] = [];
+    for (let index = 0; index < rawProposals.length; index++) {
+      proposals.push(ethers.decodeBytes32String(rawProposals[index][0]));
+    };
+    return proposals;
   }
 }
